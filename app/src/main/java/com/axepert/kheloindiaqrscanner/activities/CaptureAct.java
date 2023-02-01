b6cbf455ff36fe2d1073b8fd5fc5f63aa64cf5df
Harsh-Jajoria <harsh8725398@gmail.com>
2023-02-02 02:36:59 +0530

    02-02-2023

--- app/src/main/java/com/axepert/kheloindiaqrscanner/activities/CaptureAct.java
+++ app/src/main/java/com/axepert/kheloindiaqrscanner/activities/CaptureAct.java
@@ -2,5 +2,9 @@ package com.axepert.kheloindiaqrscanner.activities;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+
+// QR Scanner class (Don't delete this one)
+
+
 public class CaptureAct extends CaptureActivity {
 }
